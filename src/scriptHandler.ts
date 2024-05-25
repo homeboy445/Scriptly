6615bcc409694f68b8d36da1405f752c2850408a
homeboy445 <akshitsan13@gmail.com>
2024-05-25 19:04:27 +0530

    Modifier the API interface

--- src/scriptHandler.ts
+++ src/scriptHandler.ts
@@ -1,39 +1,64 @@
 import { isElementNotDefined } from "./Utility/utils";
 import domHandler, { ElementType } from "./domHandler";
-import { GenericObject, LoadPriority, ScriptEntry, ScriptStore } from "./types/types";
+import {
+  GenericObject,
+  LoadPriority,
+  ScriptEntry,
+  ScriptStore,
+} from "./types/types";
 
 class ScriptHandler {
   scriptStore: ScriptStore = [];
 
   private addScriptConfig(scriptConfig: ScriptEntry): void {
     for (const key in scriptConfig) {
-        const propertyName = key as keyof ScriptEntry;
-        if (isElementNotDefined(scriptConfig[propertyName])) {
-            delete scriptConfig[propertyName];
-        }
+      const propertyName = key as keyof ScriptEntry;
+      if (isElementNotDefined(scriptConfig[propertyName])) {
+        delete scriptConfig[propertyName];
+      }
     }
-    this.scriptStore.push(scriptConfig);
+    this.scriptStore.push({
+      ...scriptConfig,
+      attributes: Object.assign({ async: true }, scriptConfig.attributes),
+    });
   }
 
-  private addExternalScript(srcUrl: string, attributes: GenericObject, priority = LoadPriority.MEDIUM): void {
+  private addExternalScript(
+    srcUrl: string,
+    attributes: GenericObject,
+    priority: LoadPriority
+  ): void {
     this.addScriptConfig({ src: srcUrl, attributes, priority });
   }
 
-  private addInlineScript(inlineCode: string, attributes: GenericObject, priority = LoadPriority.MEDIUM) {
+  private addInlineScript(
+    inlineCode: string,
+    attributes: GenericObject,
+    priority: LoadPriority
+  ) {
     this.addScriptConfig({ inlineCode, attributes, priority });
   }
 
   public load() {
     for (let idx = 0; idx < this.scriptStore.length; idx++) {
-        domHandler.append(this.scriptStore[idx], ElementType.SCRIPT);
+      domHandler.append(this.scriptStore[idx], ElementType.SCRIPT);
     }
   }
 
   public init() {
+    const _this = this;
     return {
-      addSrc: this.addExternalScript.bind(this),
-      addInline: this.addInlineScript.bind(this)
-    };
+        add: (attributes: GenericObject = {}, priority = LoadPriority.MEDIUM) => {
+            return {
+                src: (srcUrl: string) => {
+                    _this.addExternalScript(srcUrl, attributes, priority)
+                },
+                inlineCode: (inlineCode: string) => {
+                    _this.addInlineScript(inlineCode, attributes, priority)
+                },
+            }
+        }
+    }
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       },
     ],
   },
+  mode: "development",
   resolve: {
     extensions: ['.ts', '.js'],
   },
