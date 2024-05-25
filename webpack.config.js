7e0dd5bd8c3a3e01a45ec1253a68e93e1e7c0b27
homeboy445 <akshitsan13@gmail.com>
2024-05-25 21:15:45 +0530

    Changed package name

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    library: 'scriptly'
+    library: 'scriptOrch'
   },
   plugins: [
     new CleanWebpackPlugin(),
